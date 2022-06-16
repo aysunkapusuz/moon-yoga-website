62a2f47dac7dcb4171e5e024e03af87bea7b61ba
Aysun <aysunkapusuz@gmail.com>
2022-06-16 09:45:44 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let button = document.getElementById('button')
+let backgroundImage = document.getElementById('bImg')
+let image = document.getElementById('moonn')
+
+
+function toggleBtn(){
+    button.classList.toggle('active')
+    backgroundImage.classList.toggle('backgroundImage')
+    image.classList.toggle('moon')
+    
+}
+
+// function toggleClock() {
+//     // get the clock
+//     var myClock = document.getElementById('clock');
+// 
+//     // get the current value of the clock's display property
+//     var displaySetting = myClock.style.display;
+// 
+//     // also get the clock button, so we can change what it says
+//     var clockButton = document.getElementById('clockButton');
+// 
+//     // now toggle the clock and the button text, depending on current state
+//     if (displaySetting == 'block') {
+//       // clock is visible. hide it
+//       myClock.style.display = 'none';
+//       // change button text
+//       clockButton.innerHTML = 'Show clock';
+//     }
+//     else {
+//       // clock is hidden. show it
+//       myClock.style.display = 'block';
+//       // change button text
+//       clockButton.innerHTML = 'Hide clock';
+//     }
+// }
