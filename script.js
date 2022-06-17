827b9e8c2602903bbf688aaa5f760f765c3971f3
Aysun <aysunkapusuz@gmail.com>
2022-06-17 07:32:15 -0600

    another solution added

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let button = document.getElementById('button')
 let backgroundImage = document.getElementById('bImg')
 let image = document.getElementById('moonn')
+let welcome = document.getElementById('wel')
 
 
 function toggleBtn(){
     button.classList.toggle('active')
     backgroundImage.classList.toggle('backgroundImage')
-    image.classList.toggle('moon')
+    welcome.classList.toggle('welcome')
+    // image.classList.toggle('moon')
+    if (img) {
+        document.getElementById('moonn').src = 'moon.jpg';
+      } else{
+        document.getElementById('moonn').src = '';
+        
+      }
     
 }
