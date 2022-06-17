e5d4eea9f871a9160db7e88d6546ea98794f5260
Aysun <aysunkapusuz@gmail.com>
2022-06-17 12:08:40 -0600

    another solution added

--- script.js
+++ script.js
@@ -1,13 +1,62 @@
 let button = document.getElementById('button')
 let backgroundImage = document.getElementById('bImg')
 let image = document.getElementById('moonn')
-let welcome = document.getElementById('wel')
+let welcome = document.querySelector('#wel')
 
 
 function toggleBtn(){
     button.classList.toggle('active')
     backgroundImage.classList.toggle('backgroundImage')
+    // image.classList.toggle('moon')
     welcome.classList.toggle('welcome')
-    image.classList.toggle('moon')
     
 }
+
+// document.querySelector('#button').addEventListener('click', hide)
+// function hide(){
+//   document.querySelector('#moonn').style.display = 'none'
+// }
+// const backgroundImage = document.querySelector('#bImg')
+// const image = document.querySelector('#moonn')
+// const welcome = document.querySelector('#wel')
+// const button = document.querySelector('#button')
+// 
+// document.querySelector('#button').addEventListener('click', btn)
+// function btn(){
+//   button.classList.toggle('active')
+//   image.classList.toggle('backgroundImage')
+//   welcome.classList.toggle('welcome')
+//   
+// }
+// document.querySelector('#bImg').addEventListener('click', image)
+// function image(){
+//   button.classList.toggle('backgroundImage')
+//   
+// }
+
+
+// const andi = document.querySelector('#andi')
+// const claire = document.querySelector('#claire')
+// const sharleen = document.querySelector('#sharleen')
+// 
+// document.querySelector('#andiNext').addEventListener('click', andiNext)
+// document.querySelector('#claireNext').addEventListener('click', claireNext)
+// document.querySelector('#sharleenNext').addEventListener('click', sharleenNext)
+// 
+// function andiNext(){
+// 	claire.classList.add('hidden')
+// 	sharleen.classList.add('hidden')
+// 	andi.classList.toggle('hidden')
+// }
+// 
+// function claireNext(){
+// 	andi.classList.add('hidden')
+// 	sharleen.classList.add('hidden')
+// 	claire.classList.toggle('hidden')
+// }
+// 
+// function sharleenNext(){
+// 	andi.classList.add('hidden')
+// 	claire.classList.add('hidden')
+// 	sharleen.classList.toggle('hidden')
+// }
